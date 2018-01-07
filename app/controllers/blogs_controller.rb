f4b2267d0d795d4465ac6d0a9d275ea66b6f9a5c
Bryan Lund <bryan.helge@gmail.com>
2018-01-07 14:18:10 -0700

    added test for navigating from index to show, added show page and pulled content form blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,24 @@
 class BlogsController < ApplicationController
+  before_action :get_blog, only: [:show]
 
   def index
+    @blogs = Blog.all
   end
 
   def new
+  end
+
+  def show
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :description, :image)
+  end
+
+  def get_blog
+    @blog = Blog.find(params[:id])
   end
 
 end
--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -5,6 +5,12 @@ describe 'navigation' do
   describe 'index' do
 
     before do
+    @blog = Blog.create!(title: "index_blog",
+                         description: "blog description",
+                         image: Faker::LoremPixel.image("600x300"))
+    @blog2 = Blog.create!(title: "second_index_blog",
+                         description: "blog description",
+                         image: Faker::LoremPixel.image("600x300"))
       visit blogs_path
     end
 
@@ -12,8 +18,14 @@ describe 'navigation' do
       expect(page.status_code).to eq(200)
     end
 
-    it 'has title of blogs' do
-      expect(page).to have_content(/Blogs/)
+    it 'has blogs displayed' do
+      expect(page).to have_content(/index_blog|second_index_blog/)
+    end
+
+    it 'has a link to each view blog' do
+      click_on("#{@blog.id}_show_link")
+      expect(page).to have_content(/index_blog/)
+      expect(page).to_not have_content(/second_index_blog/)
     end
   end
 
@@ -30,7 +42,7 @@ describe 'navigation' do
     it 'has title New Blog' do
       expect(page).to have_content(/New Blog/)
     end
-    
+
   end
 
 
