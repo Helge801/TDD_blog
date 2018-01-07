8c048e38f905e9c787d37c51ded7c7ded5a7601c
Bryan Lund <bryan.helge@gmail.com>
2018-01-07 13:10:45 -0700

    added Faker gem and updated seeds file to create Blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def em string
+  puts "*" * 100
+  puts "   " + string
+  puts "*" * 100
+end
+
+def random_quote
+  case [*1..6].sample
+  when 1 then Faker::HitchhikersGuideToTheGalaxy.quote
+  when 2 then Faker::HeyArnold.quote
+  when 3 then Faker::HowIMetYourMother.quote
+  when 4 then Faker::Movie.quote
+  when 5 then Faker::Seinfeld.quote
+  else
+    Faker::Dune.quote
+  end
+end
+
+def get_long_content quotes
+  content = ""
+  quotes.times do
+    content += (random_quote + " ")
+  end
+  content
+end
+
+em "creating Blogs"
+@count = 0
+20.times do |i|
+  blog = Blog.create!(title: Faker::Book.title,
+               description: get_long_content(9),
+               image:Faker::LoremPixel.image("600x300")
+              )
+  @count += 1
+  puts "#{blog.id}: #{blog.title}"
+end
+em "#{@count} blogs created"
