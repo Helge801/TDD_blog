8ffb688a2dbb0e8f3754f9079c4dafc9b0f9e22e
Bryan Lund <bryan.helge@gmail.com>
2018-01-07 21:16:40 -0700

    added tests and functionality for blog creation and edit via html form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,15 +1,33 @@
 class BlogsController < ApplicationController
-  before_action :get_blog, only: [:show]
+  before_action :get_blog, only: [:show, :edit, :update]
 
   def index
     @blogs = Blog.all
   end
 
   def new
+    @blog = Blog.new
   end
 
   def show
     @posts = @blog.post.all
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to @blog, notice: 'Blog saved'
+    else
+      render :edit
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @blog.update(blog_params)
+    redirect_to @blog, notice: 'Blog updated'
   end
 
   private
--- spec/features/blog_spec.rb
+++ spec/features/blog_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe 'navigation' do
-
   describe 'index' do
 
     before do
@@ -30,7 +29,6 @@ describe 'navigation' do
   end
 
   describe 'new' do
-      
     before do
       visit new_blog_path
     end
@@ -71,8 +69,60 @@ describe 'navigation' do
       expect(page).to have_content(/66c8c76d76g9s59|77da58d587asdf87a6f/)
     end
 
+  end
+end
 
+describe 'create via html' do
+  describe 'new' do
+    it 'blog can be created via form' do
+      visit new_blog_path
+
+      fill_in 'blog[title]', with: 'jj7g6f5e78f'
+      fill_in 'blog[description]', with: 'jsjd6dg3946555'
+      fill_in 'blog[image]', with: Faker::LoremPixel.image("600x300")
+
+      click_on 'Save'
+
+      expect(page).to have_content(/jj7g6f5e78f|jsjd6dg3946555/)
+    end
   end
 
+  describe 'edit' do
+    before do
+    @blog = Blog.create!(title: "hd87fa378q",
+                         description: "dhdf98763h37",
+                         image: Faker::LoremPixel.image("600x300"))
+    visit edit_blog_path(@blog)
+    end
 
+    it 'can be reached' do
+      expect(page.status_code).to eq(200)
+    end
+
+    it 'has pre-filled form' do
+      expect(page).to have_content(/hd87fa378q|dhdf98763h37/)
+    end
+
+    it 'can be edited via form' do
+      fill_in 'blog[title]', with: 'dhafrqrq878dfa'
+      fill_in 'blog[description]', with: 'alki255634kjr'
+      fill_in 'blog[image]', with: Faker::LoremPixel.image("600x300")
+
+      click_on 'Save'
+
+      expect expect(page).to have_content(/dhafrqrq878dfa|alki255634kjr/)
+      expect(page).to_not have_content(/Save/)
+    end
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
